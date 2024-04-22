3210f8153a18cf6c03987775e8f2a393d3f9b4c6
DudekulaShaikshavali <160754864+DudekulaShaikshavali@users.noreply.github.com>
2024-04-22 14:25:30 +0530

    Create OddandEven.java

---
+++ src/test/OddandEven.java
@@ -0,0 +1,10 @@
+public class OddandEven {
+  public static void main(String[]args){
+    for(int i=0;i<10;i++){
+      if(i%2==0){
+        System.out.println("list of even numbers:"+i);
+    }else if(i % 2 != 0) {
+          System.out.println("list of odd numbers:"+i);  
+      }
+  }
+}
