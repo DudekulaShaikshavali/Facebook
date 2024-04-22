f4622a485f51ce338a2d3278658317beea2d9862
DudekulaShaikshavali <160754864+DudekulaShaikshavali@users.noreply.github.com>
2024-04-22 14:35:49 +0530

    Create ArrayMax.java

---
+++ src/main/java/org/example/ArrayMax.java
@@ -0,0 +1,16 @@
+public class ArrayMax {
+  public static void main(String[]args){
+    int arr[]={5,2,3,4,7};
+    int max=0;
+    int min=0;
+    for(int i=0;i<arr.length;i++){
+      if(arr[i] > max){
+        max=arr[i];
+    }else if(arr[i] < min ){
+        min=arr[i];
+    }
+    }
+    System.out.println("Array maximum:"+max);
+     System.out.println("Arrayminimum:"+min);
+  }
+}
